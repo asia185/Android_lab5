464f56dc1ba2df2769d81cf7dba1ae6a8f4311f6
Joanna Tarasiuk <tarasiukjoanna@gmail.com>
2020-06-16 09:16:34 +0200

    commit

--- app/src/main/java/com/example/labki4/DodajWpis.java
+++ app/src/main/java/com/example/labki4/DodajWpis.java
@@ -5,22 +5,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class DodajWpis extends AppCompatActivity {
+    private Integer modyfi_id;
 
-    public void wyslij(View wiew){
-        EditText kontrolka = (EditText)findViewById(R.id.editTextTextPersonName);
-        String pole = kontrolka.getText().toString();
+    public void wyslij (View view){
+        EditText kolor = (EditText) findViewById(R.id.editKolor);
+        EditText wielkosc = (EditText) findViewById (R.id.editWielkosc);
+        EditText opis = (EditText) findViewById(R.id.editOpis);
+        Spinner gatunek = (Spinner) findViewById(R.id.gatunekSpinner);
+        Animal zwierz = new Animal(
+                gatunek.getSelectedItem().toString(),
+                kolor.getText().toString(),
+                Float.valueOf(wielkosc.getText().toString()),
+                opis.getText().toString()
+        );
+        zwierz.setId(this.modyfi_id);
         Intent intencja = new Intent();
-        intencja.putExtra("wpis",pole);
+        intencja.putExtra("newAnimal", zwierz);
         setResult(RESULT_OK, intencja);
-        finish();;
+        finish();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dodaj_wpis);
+        ArrayAdapter gatunki = new ArrayAdapter(this,  android.R.layout.simple_spinner_dropdown_item,
+                new String[] {"Pies", "Kot", "Rybki"});
+        Spinner gatunek = (Spinner) findViewById(R.id.gatunekSpinner);
+        gatunek.setAdapter(gatunki);
+
+        Bundle extras = getIntent().getExtras();
+        try {
+            if(extras.getSerializable("element") != null) {
+                Animal zwierz = (Animal) extras.getSerializable("element");
+                EditText kolor = (EditText) findViewById(R.id.editKolor);
+                EditText wielkosc = (EditText) findViewById (R.id.editWielkosc);
+                EditText opis = (EditText) findViewById(R.id.editOpis);
+                kolor.setText(zwierz.getKolor());
+                wielkosc.setText(Float.toString(zwierz.getWielkosc()));
+                opis.setText(zwierz.getOpis());
+                this.modyfi_id=zwierz.getId();
+            }
+        }catch(Exception ex) {
+            this.modyfi_id=0;
+        }
     }
 }
--- app/src/main/java/com/example/labki4/MainActivity.java
+++ app/src/main/java/com/example/labki4/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.labki4;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,15 +8,22 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-private ArrayList<String> target;
-private ArrayAdapter adapter;
+    private ArrayList<String> target;
+    private SimpleCursorAdapter adapter;
+    private MySQLite db = new MySQLite(this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,9 +31,38 @@ private ArrayAdapter adapter;
         String[] values = new String[]{"Pies", "Kot", "Koń", "Gołąb", "Kruk", "Dzik", "Karp", "Osioł", "Chomik", "Mysz", "Jeż", "Karaluch"};
         this.target = new ArrayList<String>();
         this.target.addAll(Arrays.asList(values));
-        this.adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,this.target);
-        ListView listView = (ListView) findViewById(R.id.animalListView);
+        this.adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, db.lista(),
+                new String[] {"_id", "gatunek"},
+                new int[]{android.R.id.text1, android.R.id.text2},
+                SimpleCursorAdapter.IGNORE_ITEM_VIEW_TYPE);
+
+        ListView listView = findViewById(R.id.animalListView);
         listView.setAdapter(this.adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView name = view.findViewById(android.R.id.text1);
+                Animal zwierz = db.pobierz(Integer.parseInt(name.getText().toString()));
+
+                Intent intencja = new Intent(getApplicationContext(), DodajWpis.class);
+                intencja.putExtra("element", zwierz);
+                startActivityForResult(intencja,2);
+            }
+        });
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView name = view.findViewById(android.R.id.text1);
+                Animal zwierz = db.pobierz(Integer.parseInt(name.getText().toString()));
+                db.usun(String.valueOf(zwierz.getId()));
+                adapter.changeCursor(db.lista());
+                adapter.notifyDataSetChanged();
+                Snackbar.make(view, "usunięto " + zwierz.getGatunek(), Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                return true;
+            }
+        });
+
     }
 
     @Override
@@ -37,18 +74,31 @@ private ArrayAdapter adapter;
     }
 
     public void nowyWpis(MenuItem menuItem){
-        Intent intent = new Intent(this, DodajWpis.class);
-        startActivityForResult(intent, 1);
+        Intent intencja = new Intent(this, DodajWpis.class);
+        startActivityForResult(intencja, 1);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && requestCode == RESULT_OK) {
+
+        if (requestCode == 1 && resultCode == RESULT_OK){
             Bundle extras = data.getExtras();
-            String nowy = (String) extras.get("wpis");
-            target.add(nowy);
+//            String nowy = (String) extras.get("wpis");
+//            target.add(nowy);
+            Animal newAnimal = (Animal) extras.getSerializable("newAnimal");
+            this.db.dodaj(newAnimal);
+            adapter.changeCursor(db.lista());
             adapter.notifyDataSetChanged();
         }
+
+        if (requestCode == 2 && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Animal newAnimal = (Animal) extras.getSerializable("newAnimal");
+            this.db.aktualizuj(newAnimal);
+            adapter.changeCursor(db.lista());
+            adapter.notifyDataSetChanged();
+        }
+
     }
 }
